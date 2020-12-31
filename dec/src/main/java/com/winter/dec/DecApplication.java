1f6e7bcf0d42b3a348f5ddeb80b2112a30fa9386
rajyawardhan1 <rajyawardhan.berlin@gmail.com>
2021-01-01 01:27:53 +0530

    first

---
+++ dec/src/main/java/com/winter/dec/DecApplication.java
@@ -0,0 +1,13 @@
+package com.winter.dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecApplication.class, args);
+	}
+
+}
